c16bf15001305cef1543b8619fcb57c660cc7e49
Sujan804 <sujanmec804@gmail.com>
2022-12-22 16:16:46 +0600

    backend fronted connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Zinziram</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>ZinZiram</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </main>
     </BrowserRouter>
   );
 }
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,28 +1,64 @@
-import React from "react";
-import data from "../data";
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
+import logger from "use-reducer-logger";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 const HomeScreen = () => {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const res = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: res.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <main>List Products</main>
       <main>
         <h1>Featured Products</h1>
         <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
+          {loading ? (
+            <div>loading</div>
+          ) : error ? (
+            <div>{{ error }}</div>
+          ) : (
+            products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>{" "}
+                  <img src={product.image} alt={product.name} />
                 </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to card</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>{" "}
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to card</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </main>
     </div>
